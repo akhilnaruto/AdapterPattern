7496a5e28110eb0ea87cc08fdc450090b3dd3088
Akhil <akhil.munna02@gmail.com>
2015-07-18 17:54:41 +0530

    1. Added Data renderer class 2. added test for data renderer

---
+++ AdapterPattern/Test/DataRendererTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern.Test
+{
+    [TestClass]
+    public class DataRendererTest
+    {
+        [TestMethod]
+        public void RenderOneRowGivenStubDataAdapter()
+        {
+            var myRenderer = new DataRenderer(new StubDbAdapter());
+
+            var writer = new StringWriter();
+            myRenderer.Render(writer);
+
+            string result = writer.ToString();
+            Console.Write(result);
+
+            int lineCount = result.Count(c => c == '\n');
+            Assert.AreEqual(3, lineCount);
+        }
+
+    }
+}
